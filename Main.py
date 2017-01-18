a674bc6de6bc702d9572820c67d00b03de5b50d5
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2017-01-17 19:32:26 -0600

    Added some funny little stuff generating states in Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,27 @@
 from Vehicle import Vehicle
 from Package import Package
+import random
 
 if __name__ == "__main__":
-    print("Hello.")
 
+    random.seed(0)
+
+    locations = []
     packages = []
     vehicles = []
 
-    for i in range(10):
-        vehicles.append(Vehicle(i, i))
-        packages.append(Package(i, i, i))
+    M = 20
+    K = 10
+    V = 5
+
+    Garage = random.randrange(0, M)
+
+    for i in range(M):
+        locations.append(i)
+    for i in range(V):
+        vehicles.append(Vehicle(i, Garage))
+    for i in range(K):
+        src = random.randrange(0, M)
+        dest = random.randrange(0, M)
+        packages.append(Package(i, src, dest))
 
