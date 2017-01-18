526143cbeb3c05966ca3a3d6854f802d41625603
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2017-01-17 19:24:21 -0600

    Added methods dropOffPkg for leaving packages behind, and added comments to all methods in Vehicle.

--- Vehicle.py
+++ Vehicle.py
@@ -28,5 +28,68 @@ class Vehicle:
         paths of all vehicles in the simulation. """
         self._path = []
 
+    def getId(self):
+        """
+        Return the unique id of this Vehicle.
+        :rtype: int
+        """
+        return self._id
 
+    def getPkg(self):
+        """
+        Return the contents of this vehicle.  If this Vehicle has
+        a package, return the id of that package.  If the vehicle
+        is empty, return None.
+        :rtype: int, None
+        """
+        return self._pkg
+
+    def setPkg(self, pkg):
+        """
+        Pickup a package.  This assigns a package as the contents of
+        the vehicle.  Return True if this is successful, and False if
+        there is already a package in the vehicle.
+        :param pkg: A package id.
+        :type pkg: int
+        :rtype: bool
+        """
+        if self._pkg is None:
+            self._pkg = pkg
+            return True
+        else:
+            return False
+
+    def dropOffPkg(self):
+        """
+        Drop off a package.  Because the vehicle does not know all
+        about the graph world it drives in, this just means emptying its
+        contents.  Return True if successful, and False if the Vehicle
+        doesn't have a package to begin with.
+        :rtype: bool
+        """
+        if self._pkg is None:
+            return False
+        else:
+            self._pkg = None
+            return True
+
+    def getDest(self):
+        """
+        Return the current destination of this Vehicle,
+        or None if the Vehicle has no destination.
+        :rtype: int, None
+        """
+        return self._dest
+
+    def setDest(self, dest):
+        """
+        Give this vehicle a current destination.
+        """
+        self._dest = dest
+
+    def addToPath(self, loc):
+        """
+        Not sure if this will be any use.
+        """
+        self._path.append(loc)
 
