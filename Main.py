7942cc06bba93f3bed0a34fb43b37e4f3c4005f6
Francois Roewer-Despres <francois-rd>
2017-01-23 15:50:30 -0600

    Removed useless imports in Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,4 @@
 if __name__ == "__main__":
-    import networkx as nx
-    import matplotlib.pyplot as plt
-    import graphs
-    import World
     import test_graphs as tg
-
     tg.test_graphs()
+
