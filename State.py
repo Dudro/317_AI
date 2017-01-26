dc5104aa181db77b4b8c553b5f1bea5d604a5ac3
Hoang Anh Tu Nguyen <LavieNguyen@uofs-10-227-132-27.usask.ca>
2017-01-25 20:19:18 -0600

    Fixed typos in State transition function.

--- State.py
+++ State.py
@@ -1,4 +1,5 @@
 import World as world_state
+
 class State:
     """
     A State simply represents a state in the state space.
@@ -68,40 +69,27 @@ def stateTransition(state):
     number_of_cars = state.get_number_of_cars();
     undelivered = state.get_num_undelivered();
     for i in range(0, number_of_cars):
-	for cars in combinations(number_of_cars,i):
-		for packs_perm in permutations(state.get_undelivered_packages(), i):
-			car_with_pack = [-1] * number_of_cars
-			#deep copy this, or shallow?
-			new_car_locs = copy.deepcopy(state.get_car_locs())
-			new_packages = copy.deepcopy(state.get_packages())
-			new_g = state.get_g()
-			for j in range(0, i):
-				car_with_pack[cars[j]]=packs_perm[j]
-				#update the values of the list state.get_carLocs() for the new state
-				new_car_locs[cars[j]] = world_state.get_package_dest(car_with_pack[cars[j]])				
-				#update the values of the list state.get_packages() for the new state
-				if car_with_pack[j] != -1:
-					new_packages[car_with_pack[j]]=True
-
-				new_g += world_state.get_path_cost(state.get_car_locs()[cars[j]], world_state.get_package_source(packs_perm[j]))
-				new_g += world_state.get_path_cost(state.get_car_locs()[cars[j]], world_state.get_package_dest(packs_perm[j]))
-			#new_state is assigned to updated state of carLocs and packages	
-	
-			new_state = State(new_car_locs, new_packages, new_g)
-
-    #go get the world data from somewhere
-    #for i in range(1, len(state.get_packages()) - state.get_num_delivered() + 1):
-        #for each  combination of N pick i
-            #for each permutation of i packages
-                #calculate the sum of path costs for this permutation
-                #g = state.get_g() + path_cost_sum
-                #update the values of the list state.get_carLocs() for the new state
-                #update the values of the list state.get_packages() for the new state
-                #new_state = State(newLocs, newPackages, g)
-    
-                #calculate new_state's h() with the problem's chosen heuristic                
-
-                #compare new_state with fringe
-                #if it's not a duplicate or if it's a cheaper duplicate
-                    #successors.append(new_state)
+        for cars in combinations(number_of_cars,i):
+	    for packs_perm in permutations(state.get_undelivered_packages(), i):
+		car_with_pack = [-1] * number_of_cars
+		#deep copy this, or shallow?
+		new_car_locs = copy.deepcopy(state.get_car_locs())
+		new_packages = copy.deepcopy(state.get_packages())
+		new_g = state.get_g()
+		for j in range(0, i):
+		    car_with_pack[cars[j]]=packs_perm[j]
+		    #update the values of the list state.get_carLocs() for the new state
+		    new_car_locs[cars[j]] = world_state.get_package_dest(car_with_pack[cars[j]])				
+		    #update the values of the list state.get_packages() for the new state
+		    if car_with_pack[j] != -1:
+		        new_packages[car_with_pack[j]]=True
+                    new_g += world_state.get_edge_cost( \
+                            state.get_car_locs()[cars[j]], \
+                            world_state.get_package_source(packs_perm[j]))
+		    new_g += world_state.get_edge_cost( \
+                            world_state.get_package_source(packs_perm[j]), \
+                            world_state.get_package_dest(packs_perm[j]))
+		    # new_state is added to list of successors		
+		    new_state = State(new_car_locs, new_packages, new_g)
+                    successors.append(new_state)
     return successors
