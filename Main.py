50099e0ae64ee7eed82bbf49cfe4e1b6311e611d
Hoang Anh Tu Nguyen <LavieNguyen@uofs-10-227-132-27.usask.ca>
2017-01-25 20:41:57 -0600

    Added goal function to Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,22 @@
-if __name__ == "__main__":
-    import test_graphs as tg
-    tg.test_graphs()
+import State as s
+import World as w 
+import test_graphs as tg
 
+def is_goal(state):
+    """
+    :param state: A particular State
+    :type state: State
+    :rtype: True if this state is a goal state,
+        False otherwise.
+    """
+    for p in state.get_packages():
+        if p is False:
+            return False
+    return True
+
+if __name__ == "__main__":
+        tg.test_graphs()
+
+    
+
+    
