76e73cc92d2cd308aae2701e6fc6e528948f76cf
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2017-01-26 22:49:00 -0600

    Added astar search for optimal solution only with circle graph.

--- Main.py
+++ Main.py
@@ -32,6 +32,16 @@ def a_star_any_graph(n, k, m, full_map, pairs, f):
         print(solution.get_g(), solution.get_car_locs())
 
 
+def a_star_optimal_any_graph(n, k, m, full_map, pairs, f):
+    world = World(n, k, m, full_map, pairs)
+    world.process_map()
+    cars = [[world.get_garage()]] * n
+    packages = [False] * k
+    initial = State(world, cars, packages, 0)
+    solution = next(a_star(initial, is_goal, state_transition, f))
+    print(solution.get_g(), solution.get_car_locs())
+
+
 def a_star_triangle_graph(n, k, f):
     print("Starting triangle test", flush=True)
     full_map, pairs = graphs.get_triangle_graph()
@@ -50,13 +60,14 @@ def a_star_circle_graph(n, f):
     print("Starting circle test", flush=True)
     full_map, pairs = graphs.get_circle_graph()
     k = len(pairs)
-    #a_star_any_graph(n, k, full_map.number_of_nodes(), full_map, pairs, f)
+    a_star_optimal_any_graph(
+            n, k, full_map.number_of_nodes(), full_map, pairs, f)
     print("Done circle", flush=True)
 
 
 if __name__ == "__main__":
-    #a_star_triangle_graph(1, 2, decorating_f(State.zero_h))
-    #a_star_ogg_graph(1, 3, decorating_f(State.zero_h))
+    a_star_triangle_graph(1, 2, decorating_f(State.zero_h))
+    a_star_ogg_graph(1, 3, decorating_f(State.zero_h))
     a_star_circle_graph(1, decorating_f(State.zero_h))
     # a_star_triangle_graph(2, 2, decorating_f(State.zero_h))
     # a_star_ogg_graph(2, 3, decorating_f(State.zero_h))
