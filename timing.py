55590bd6cfc89704623213f815c51cae46c037c1
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2017-01-28 21:22:53 -0600

    Adjusted timing for many simulations and added json output and plotting formulation

--- timing.py
+++ timing.py
@@ -1,27 +1,28 @@
 from functools import reduce
 from time import clock
 
-start_t = []
+start_t = {}
 
 def secondsToStr(t):
     return "%d:%02d:%02d.%03d" % \
         reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],
             [(t*1000,),1000,60,60])
 
-def start_timer():
+def start_timer(i):
     """
     Call this to start the timer.
     This is process execution time, not wall time.
     """
-    start_t.append(clock())
+    start_t[i] = clock()
                 
 def end_timer(i):
     """
     Call this to end the timer and get the
     execution time in a String.
     """
-    return clock()-start_t[i]
+    return clock() - start_t[i]
 
 def now():
     return secondsToStr(clock())
 
+    
--- utils.py
+++ utils.py
@@ -58,5 +58,39 @@ def combinations_list(numbers, r):
                 ret.extend(comb)
                 yield ret
 
+
+def filter_pairs(pairs):
+    valid_pairs = []
+    for sd in pairs:
+        if sd[0] != sd[1]:
+            valid_pairs.append(sd)
+    return valid_pairs
+
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def output_plot(path, data):
+    from plotly import offline as py
+    from plotly import graph_objs as go
+
+    plot_data = {}
+    for key in data[0].keys():
+        plot_data[key] = {
+            'x': [],
+            'y': [],
+            'mode': 'lines+markers',
+            'name': key
+        }
+
+    for i, d in enumerate(data):
+        for k, v in d.items():
+            plot_data[k]['x'].append(i)
+            plot_data[k]['y'].append(v)
+
+    to_plot = []
+    for k in sorted(plot_data.keys()):
+        to_plot.append(go.Scatter(plot_data[k]))
+
+    py.plot(to_plot, filename=path, auto_open=False)
