a48f3bab0420f148bdb91e6b552c8a94cf491b36
Francois Roewer-Despres <francois-rd>
2017-01-30 18:43:47 -0600

    Cleaned up State.py, moved get_number_of_cars() into World.py, and added docstrings to State.py.

--- State.py
+++ State.py
@@ -27,9 +27,19 @@ class State:
         self._car_locs = car_locs
         self._packages = packages
         self._g = cost_so_far
-        self._h = 0
 
     def __eq__(self, other):
+        """
+        Two states are considered equal if they have the same number of cars,
+        if the *current* location of each car is the same in both states, and
+        if the same packages have been delivered. The path each car has taken
+        to reach its current location, and the order in which packages were
+        delivered (and which car delivered them) is *not* considered for
+        determining state equality.
+        :param other: another state
+        :type other: State
+        :rtype: bool
+        """
         cars = self.get_car_locs()
         other_cars = other.get_car_locs()
         for i in range(len(cars)):
@@ -44,59 +54,99 @@ class State:
         return True
 
     def get_world(self):
+        """
+        Returns the world state
+        :rtype: World
+        """
         return self._world
 
     def get_g(self):
+        """
+        Returns g(x), the cost so far.
+
+        In A*, states are examined in order of least f-value, where, for any
+        state x, f(x) = g(x) + h(x), where g(x) is the total cost so far from
+        the initial state to state x, and h(x) is the heuristic estimating the
+        remaining cost still to be incurred between x and a goal state.
+        :rtype: float
+        """
         return self._g
-
-    def get_h(self):
-        return self._h
-
-    def set_h(self, new):
-        self._h = new
-
-    def get_number_of_cars(self):
-        return len(self._car_locs)
 
     def get_car_locs(self):
         """
-        Returns the stack of the car paths over time.
+        For each car, returns a stack recording the car's path over time.
+        :rtype: list(list(int))
         """
         return self._car_locs
 
     def get_packages(self):
+        """
+        Returns a list of booleans showing whether each package has been
+            delivered or not
+        :rtype: list(bool)
+        """
         return self._packages
 
     def get_car_path(self, n):
-        if n >= len(self._car_locs):
-            return None
+        """
+        Returns a stack recording the n'th car's path over time.
+        :param n: the index of the car
+        :rtype: list(int)
+        """
         return self._car_locs[n]
 
     def get_car_loc(self, n):
-        return self.get_car_path(n)[len(self.get_car_path(n)) - 1]
-
-    def get_package_status(self, k):
-        if k >= len(self._packages):
-            return None
-        return self._packages[k]
+        """
+        Returns the current location of the n'th car.
+        :param n: the index of the car
+        :rtype: int
+        """
+        return self._car_locs[n][len(self._car_locs[n]) - 1]
 
     def get_num_delivered(self):
+        """
+        Returns the number of packages that have already been delivered.
+        :rtype: int
+        """
         return self._packages.count(True)
 
     def get_num_undelivered(self):
+        """
+        Returns the number of packages that still need to be delivered.
+        :rtype: int
+        """
         return self._packages.count(False)
 
-    def get_undelivered_packages(self):
-        with_index = zip(range(0, len(self._packages)), self._packages)
-        return [i for (i, delivered) in with_index if delivered]
+    def all_packages_delivered(self):
+        """
+        Returns whether all packages have been delivered or not.
+        :rtype: bool
+        """
+        return self.get_num_undelivered() == 0
+
+    # Heuristics
 
     def zero_h(self):
+        """
+        The 0 heuristic. That is, h(x) = 0 for all states x.
+        :rtype: int
+        """
         return 0
 
     def undelivered_h(self):
+        """
+        A heuristic that simply counts the number of undelivered packages.
+        :rtype: int
+        """
         return self.get_num_undelivered()
 
     def sum_of_package_distance_h(self):
+        """
+        A heuristic that computes the sum of the distance between each package
+        and its destination. This is >0 for all undelivered package, and 0 for
+        all delivered packages.
+        :rtype: float
+        """
         sum_of_package_distances = 0
         for i in range(len(self._packages)):
             if not self._packages[i]:  # if package is not yet delivered
@@ -105,23 +155,44 @@ class State:
         return sum_of_package_distances
 
     def sum_of_package_distance_scaled_h(self):
+        """
+        A heuristic that computes the sum of the distance between each package
+        and its destination, and then scales down the heuristic by 1 - k/K,
+        where k is the number of packages delivered and K is the total number
+        of packages. The idea is that, as more packages are delivered, the
+        heuristic drops to 0 faster than simply by the computing the sum, and
+        this may give search algorithms more of an incentive to drop off a
+        package (since a heuristic value closer to 0 implies that we are closer
+        to a goal state).
+        :rtype: float
+        """
         sum_of_package_distances = self.sum_of_package_distance_h()
         num_delivered = self.get_num_delivered()
-        reduction_val = 1.0 / float(len(self.get_packages()))
+        reduction_val = 1.0 / float(len(self._packages))
         scalar = 1 - (num_delivered * reduction_val)
         return scalar * sum_of_package_distances
 
-    def lavie_h(self):
-        return 0
 
-
-# this function is in between of completion, Needs work on it.
-# Need to work on calculating the sum of path cost.
 def state_transition(state):
+    """
+    Returns a list of all possible successors of the given state.
+
+    The successors are as follows:
+    - if the given state has all packages delivered, then the only successor
+    state has all the cars back at the garage;
+    - otherwise, for each possible combination of cars of each size (from a
+    single car to all the cars), for each possible permutations (of the correct
+    size) of assignments of the undelivered packages to the chosen cars, we
+    create a successor state wherein the chosen cars have moved to their
+    assigned package's source (if needed), and then moved their assigned
+    package from its source to its destination.
+    :param state: the state for which the successors should be found
+    :rtype: list(State)
+    """
     successors = []
     world = state.get_world()
-    number_of_cars = state.get_number_of_cars()
-    if all_packages_delivered(state):
+    number_of_cars = world.get_number_of_cars()
+    if state.all_packages_delivered():
         new_car_locs = [[world.get_garage()]] * number_of_cars
         for n in range(number_of_cars):
             new_car_locs[n] = copy.deepcopy(state.get_car_path(n))
@@ -140,7 +211,6 @@ def state_transition(state):
         # print("Trying all combinations with just", i, "cars moving.")
         for cars in combinations(number_of_cars, i):
             # print("Trying combination:", cars)
-            # TODO: Problem? What if len(state.get_packages()) < i?
             # print("Trying all permutations of", i, "package assignments.")
             for packs_perm in permutations_exclude(
                     len(state.get_packages()), i, state.get_packages()):
@@ -178,25 +248,18 @@ def state_transition(state):
                 # print("Generated successor: ", flush=True)
                 # print(new_state.get_car_locs(), flush=True)
                 # print(new_state.get_packages(), flush=True)
-                # TODO: do we need to check that new state hasn't already been
-                #       visited? If not, what is the point of __eq__()?
                 successors.append(new_state)
     return successors
 
 
 def is_goal(state):
-    for car in range(state.get_number_of_cars()):
-        if state.get_car_loc(car) != state.get_world().get_garage():
-            return False
-    return all_packages_delivered(state)
-
-
-def all_packages_delivered(state):
     """
-    Determines whether the given state has all packages delivered.
-
-    :param state: a particular state
-    :type state: State
+    Returns whether or not the given state is a goal state. A goal state has
+    every package delivered and every car back at the garage.
+    :param state: the state to check
     :rtype: bool
     """
-    return state.get_num_undelivered() == 0
+    for car in range(state.get_world().get_number_of_cars()):
+        if state.get_car_loc(car) != state.get_world().get_garage():
+            return False
+    return state.all_packages_delivered()
