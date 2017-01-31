258c1f3aa2d76deccee6a4aefeb5b775b51e07be
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2017-01-31 16:40:01 -0600

    Added if __name__ == __main__ check to test files.

--- test_localbeam.py
+++ test_localbeam.py
@@ -1,8 +1,9 @@
 from localbeam import local_beam_search
 from test_astar import *
 
-# Generate (lazily) every solution (path) from Arad to Bucharest.
-print("Local Beam Search")
-for sol, count in local_beam_search(GraphState(2, 0, [2]), at_bucharest,
-                                    trans_op, f, 5):
-    print(count, sol.get_cost_so_far(), solution.get_path_so_far())
+if __name__ == "__main__":
+    # Generate (lazily) every solution (path) from Arad to Bucharest.
+    print("Local Beam Search")
+    for sol, count in local_beam_search(GraphState(2, 0, [2]), at_bucharest,
+                                        trans_op, f, 5):
+        print(count, sol.get_cost_so_far(), solution.get_path_so_far())
