6f141309add1b1b148f83d4db06c3089c933fe14
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2017-01-30 22:01:25 -0600

    Added docstrings to World.py.

--- World.py
+++ World.py
@@ -28,9 +28,9 @@ class World:
         :param g: the garage node. Default: 0.
         :type g: int
         """
-        self._N = n
-        self._K = k
-        self._M = m
+        self._N = n  # Python style says parameters should be lowercase...
+        self._K = k  # ... but internally, the true parameters can all be...
+        self._M = m  # ... uppercase, just like in the problem description.
         self._full_map = full_map
         self._all_pairs_shortest_paths = nx.all_pairs_shortest_path(full_map)
         self._reduced_map = None
@@ -43,39 +43,72 @@ class World:
         self._G = g
 
     def get_garage(self):
+        """
+        Returns the vertex index of the garage.
+        :rtype: int
+        """
         return self._G
 
     def get_number_of_cars(self):
+        """
+        Returns the total number of cars.
+        :rtype: int
+        """
         return self._N
 
     def get_important_vertices(self):
+        """
+        Returns a list of the important vertices of the original map. These
+        correspond to vertices that are a package source and/or a package
+        destination and/or or the garage. The resulting list is cached to speed
+        up future invocations.
+        :rtype: list(int)
+        """
         if self._important_vertices is None:
-            important = []
+            important = [self._G]  # Garage is always important.
             for src, dest in self._source_dest_pairs:
                 if src not in important:
                     important.append(src)
                 if dest not in important:
                     important.append(dest)
-            if self._G not in important:  # add garage
-                important.append(self._G)
             self._important_vertices = important
             return important
         else:
             return self._important_vertices
 
     def get_package_source(self, pkg_id):
+        """
+        Returns the source vertex of the given package.
+        :param pkg_id: the package
+        :type pkg_id: int
+        :rtype: int
+        """
         src, _ = self._source_dest_pairs[pkg_id]
         return src
 
     def get_package_dest(self, pkg_id):
+        """
+        Returns the destination vertex of the given package.
+        :param pkg_id: the package
+        :type pkg_id: int
+        :rtype: int
+        """
         _, dest = self._source_dest_pairs[pkg_id]
         return dest
 
     def process_map(self):
-        # Retrieve all pairs paths and distances
-        self.get_important_vertices()
+        """
+        Processes the full map into a reduced map containing only the important
+        vertices of the original map. Makes the reduced map a complete graph,
+        where each vertex is connected to each other vertex by an edge weighted
+        according to the cost of the shortest (i.e. lowest cost) path between
+        them.
+        """
+        # Compute all pairs of paths and their cost.
         paths = nx.floyd_warshall(self._full_map)
 
+        # Create the complete reduced map.
+        self.get_important_vertices()
         self._reduced_map = nx.Graph()
         self._reduced_map.add_nodes_from(self._important_vertices)
         for i in self._important_vertices:
@@ -85,20 +118,42 @@ class World:
 
         self._reduced_map_as_dict = nx.to_dict_of_dicts(self._reduced_map)
 
-    def get_edge_cost(self, curr_loc, goal):
-        return self._reduced_map_as_dict[curr_loc][goal]['weight']
+    def get_edge_cost(self, location, goal):
+        """
+        Returns the edge cost between any two vertices in the reduced map. The
+        reduced map is precomputed if needed.
+        :param location: the source vertex
+        :param goal: the destination vertex
+        :rtype: float
+        """
+        if self._reduced_map is None:
+            self.process_map()
+        return self._reduced_map_as_dict[location][goal]['weight']
 
     def get_reduced_map(self):
+        """
+        Returns the reduced map. The reduced map is precomputed if needed.
+        :rtype: NetworkX Graph
+        """
         if self._reduced_map is None:
             self.process_map()
         return self._reduced_map
 
     def get_shortest_path(self, source, dest):
+        """
+        Returns the shortest path between any two vertices in the original map.
+        :param source: the source vertex
+        :param dest: the destination vertex
+        :rtype: list(int)
+        """
         return self._all_pairs_shortest_paths[source][dest]
 
     def get_package_cost(self, package):
         """
         Returns the edge cost between the given package's source and
-        destination in the map.
+        destination in the reduced map.
+        :param package: the package
+        :type package: int
+        :rtype: float
         """
         return self.get_edge_cost(*self._source_dest_pairs[package])
