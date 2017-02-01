901fa37cf44a4ad02c9d33007dc1f7be5bfb3f0b
Francois Roewer-Despres <francois-rd>
2017-02-01 10:18:26 -0600

    Added verbosity argument to functions in simulations.py, and refactored Main.py to use argparse to handle command line arguments.

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 from simulation import *
 from State import State
 import utils
-import sys
+import argparse
 
+# Default values for command line arguments.
 defaults = {
     'num_sims': 25,
     'n': 2,
@@ -15,125 +16,143 @@ defaults = {
 }
 
 
-def usage(program_name, msg):
+def parse_positive_int(value):
     """
-    Prints the program name and the message (as well as a generic usage
-    message) to standard error, and then exits the program with status 1.
+    Returns the integer value of the given string, raising an error if the
+    string cannot be parsed to an integer, or if the resulting integer is not
+    positive.
 
-    :param program_name: the name of the program
-    :param msg: the message to print
+    :param value: the string to parse
+    :rtype: int
     """
-    from utils import eprint
-    eprint(program_name, ":", msg)
-    eprint("  num_sims -- The total number of simulations to run. Default:",
-           defaults['num_sims'])
-    eprint("  n        -- The number of cars for each simulation. Default:",
-           defaults['n'])
-    eprint("  k        -- The number of packages for each simulation. Default:",
-           defaults['k'])
-    eprint("  m        -- The number of locations in the input map. Default:",
-           defaults['m'])
-    eprint("  h        -- The heuristic function to use; one of zero,")
-    eprint("              undelivered, scaled, or sum. Default:",
-           defaults['h_name'])
-    eprint("  bound    -- Causes Bounded A* to keep only the best 'bound'")
-    eprint("              number of successors for any given state. Can be")
-    eprint("              either a percentage of successors to keep (if")
-    eprint("              between 0 and 1, both exclusive), or the maximum")
-    eprint("              number of successors to keep (if an integer 1 or")
-    eprint("              greater). If 0 or less, keep all successors, like")
-    eprint("              regular A*. Default:", defaults['bound'])
-    eprint("  k_limit  -- Causes Local Beam Search to consider only the best")
-    eprint("              (k_limit - 1) successors of any given state at each")
-    eprint("              level of recursion. Default:", defaults['k_limit'])
-    sys.exit(1)
+    pos = int(value)
+    if pos < 1:
+        raise argparse.ArgumentTypeError("invalid value: " + value)
+    return pos
+
+
+def parse_bound(value):
+    """
+    Returns the bound value resulting from parsing the given string, where
+    bound is one of the arguments to bounded_a_star, raising an error if the
+    value cannot be parsed to an int or a float, or if the value is greater
+    than 1, but is not an int.
+
+    :param value: the string to parse
+    :rtype: int or float
+    """
+    b = float(value)
+    if b > 1 and not b.is_integer():
+        raise argparse.ArgumentTypeError("invalid bound argument: " + value)
+    if b.is_integer():
+        return int(b)
+    else:
+        return b
+
+
+def parse_heuristic(value):
+    """
+    Returns both the heuristic function and the heuristic function name
+    corresponding to the given value, raising an error if the value is not a
+    legal heuristic function name.
+
+    :param value: the string to parse
+    :return: (State => float, string)
+    """
+    if value == "zero":
+        return State.zero_h, value
+    elif value == "undelivered":
+        return State.undelivered_h, value
+    elif value == "scaled":
+        return State.sum_of_package_distance_scaled_h, value
+    elif value == "sum":
+        return State.sum_of_package_distance_h, value
+    else:
+        raise argparse.ArgumentTypeError("invalid heuristic argument: " + value)
 
 
 if __name__ == "__main__":
-    # Set variables to default values.
-    num_sims = defaults['num_sims']
-    n = defaults['n']
-    k = defaults['k']
-    m = defaults['m']
-    h = defaults['h']
-    h_name = defaults['h_name']
-    bound = defaults['bound']
-    k_limit = defaults['k_limit']
+    # Define command line arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action='store_true',
+                        help="more verbose output")
+    parser.add_argument("-a", "--a-star", action='store_true',
+                        help="run simulations using A* Search")
+    parser.add_argument("-b", "--bounded-a-star", action='store_true',
+                        help="run simulations using Bounded A* Search")
+    parser.add_argument("-l", "--local-beam", action='store_true',
+                        help="run simulations using Local Beam Search")
+    parser.add_argument("--num_sims", type=parse_positive_int,
+                        default=defaults['num_sims'],
+                        help="total number of simulations to run")
+    parser.add_argument("-n", "--vehicles", type=parse_positive_int,
+                        default=defaults['n'],
+                        help="number of cars for each simulation")
+    parser.add_argument("-k", "--packages", type=parse_positive_int,
+                        default=defaults['k'],
+                        help="number of packages for each simulation")
+    parser.add_argument("-m", "--locations", type=parse_positive_int,
+                        default=defaults['m'],
+                        help="number of locations in the input map")
+    parser.add_argument("--heuristic", type=parse_heuristic,
+                        default=(defaults['h'], defaults['h_name']),
+                        choices=["zero", "undelivered", "scaled", "sum"],
+                        help="heuristic function to use")
+
+    parser.add_argument("--bound", type=parse_bound, default=defaults['bound'],
+                        help="causes Bounded A* to keep only the best BOUND "
+                             "number of successors for any given state; can "
+                             "be either a percentage of successors to keep "
+                             "(if between 0 and 1, both exclusive), or the "
+                             "maximum number of successors to keep (if an "
+                             "integer 1 or greater); if 0 or less, keep all "
+                             "successors, like regular A*")
+    parser.add_argument("--k-limit", type=parse_positive_int,
+                        default=defaults['k_limit'],
+                        help="causes Local Beam Search to consider only the "
+                             "best (K_LIMIT - 1) successors of any given "
+                             "state at each level of recursion")
 
     # Parse command line arguments.
-    if len(sys.argv) > 1:
-        try:
-            num_sims = int(sys.argv[1])
-            if num_sims < 1:
-                raise ValueError
-        except ValueError:
-            usage(sys.argv[0], "Invalid simulation argument: " + sys.argv[1])
-    if len(sys.argv) > 2:
-        try:
-            n = int(sys.argv[2])
-            if n < 1:
-                raise ValueError
-        except ValueError:
-            usage(sys.argv[0], "Invalid vehicle argument: " + sys.argv[2])
-    if len(sys.argv) > 3:
-        try:
-            k = int(sys.argv[3])
-            if k < 1:
-                raise ValueError
-        except ValueError:
-            usage(sys.argv[0], "Invalid package argument: " + sys.argv[3])
-    if len(sys.argv) > 4:
-        try:
-            m = int(sys.argv[4])
-            if m < 1:
-                raise ValueError
-        except ValueError:
-            usage(sys.argv[0], "Invalid location argument: " + sys.argv[4])
-    if len(sys.argv) > 5:
-        if sys.argv[5] == "zero":
-            h = State.zero_h
-            h_name = "zero"
-        elif sys.argv[5] == "undelivered":
-            h = State.undelivered_h
-            h_name = "undelivered"
-        elif sys.argv[5] == "scaled":
-            h = State.sum_of_package_distance_scaled_h
-            h_name = "scaled"
-        elif sys.argv[5] == "sum":
-            h = State.sum_of_package_distance_h
-            h_name = "sum"
-        else:
-            usage(sys.argv[0], "Invalid heuristic argument: " + sys.argv[5])
-    if len(sys.argv) > 6:
-        try:
-            bound = float(sys.argv[6])
-            if bound > 1 and not isinstance(bound, int):
-                raise ValueError
-        except ValueError:
-            usage(sys.argv[0], "Invalid bound argument: " + sys.argv[6])
-    if len(sys.argv) > 7:
-        try:
-            k_limit = int(sys.argv[7])
-            if k_limit < 1:
-                raise ValueError
-        except ValueError:
-            usage(sys.argv[0], "Invalid k limit argument: " + sys.argv[7])
+    args = parser.parse_args()
+    verbose = args.verbose
+    a_star = args.a_star
+    bounded_a_star = args.bounded_a_star
+    local_beam = args.local_beam
+    num_sims = args.num_sims
+    n = args.vehicles
+    k = args.packages
+    m = args.locations
+    h, h_name = args.heuristic
+    bound = args.bound
+    k_limit = args.k_limit
+
+    if not a_star and not bounded_a_star and not local_beam:
+        raise parser.error("at least one of -a, -b, or -l must be given")
 
     # Run the simulations and record simulation results.
-    name = "n" + str(n) + ".k" + str(k) + ".m" + str(m) + "." + h_name + "."
+    name = "sims" + str(num_sims) + ".n" + str(n) + ".k" + str(k) + ".m" + \
+           str(m) + "." + h_name + "."
 
-    print("Regular A* simulations.")
-    data_a_star = a_star_simulations(n, k, m, h, num_sims)
-    utils.dump_json_data(name + "a_star", data_a_star)
-    utils.plot_results(name + "a_star", data_a_star)
+    if verbose and a_star:
+        print("Regular A* simulations.")
+    if a_star:
+        data_a_star = a_star_simulations(n, k, m, h, num_sims, verbose)
+        utils.dump_json_data(name + "a_star", data_a_star)
+        utils.plot_results(name + "a_star", data_a_star)
 
-    print("Bounded A* simulations.")
-    data_bounded_a_star = bounded_a_star_simulations(n, k, m, h, num_sims,
-                                                     bound)
-    utils.dump_json_data(name + "bounded_a_star", data_bounded_a_star)
-    utils.plot_results(name + "bounded_a_star", data_bounded_a_star)
+    if verbose and bounded_a_star:
+        print("Bounded A* simulations.")
+    if bounded_a_star:
+        data_bounded_a_star = bounded_a_star_simulations(n, k, m, h, num_sims,
+                                                         bound, verbose)
+        utils.dump_json_data(name + "bounded_a_star", data_bounded_a_star)
+        utils.plot_results(name + "bounded_a_star", data_bounded_a_star)
 
-    print("Local Beam Search simulations.")
-    data_local_beam = local_beam_simulations(n, k, m, h, num_sims, k_limit)
-    utils.dump_json_data(name + "local_beam", data_local_beam)
-    utils.plot_results(name + "local_beam", data_local_beam)
+    if verbose and local_beam:
+        print("Local Beam Search simulations.")
+    if local_beam:
+        data_local_beam = local_beam_simulations(n, k, m, h, num_sims, k_limit,
+                                                 verbose)
+        utils.dump_json_data(name + "local_beam", data_local_beam)
+        utils.plot_results(name + "local_beam", data_local_beam)
