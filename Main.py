f1c04b204173020d3bf310f15332b316db79a8b0
Francois Roewer-Despres <francois-rd>
2017-02-01 20:49:48 -0600

    Added get_file_names() in Main.py, and refactored code to use it. Also, renamed local variables to avoid shadowing.

--- Main.py
+++ Main.py
@@ -58,91 +58,109 @@ def parse_bound(value):
         return b
 
 
+def get_file_names(num_sims=defaults['num_sims'], n=defaults['n'],
+                   k=defaults['k'], m=defaults['m'],
+                   h_name=defaults['h_name'], bound=defaults['bound'],
+                   k_limit=defaults['k_limit']):
+    """
+    Returns a dict where the keys are search algorithm names and the values are
+    the bases (i.e. the part before the extension) of the output file names,
+    which depend on the values of the given parameters.
+
+    :rtype: dict
+    """
+    name_base = "sims" + str(num_sims) + ".n" + str(n) + ".k" + str(k) + \
+                ".m" + str(m) + "." + h_name + "."
+    return {
+        'a_star': name_base + "a_star",
+        'bounded_a_star': name_base + "bound" + str(bound) + ".bounded_a_star",
+        'local_beam': name_base + "k_limit" + str(k_limit) + ".local_beam"
+    }
+
+
 if __name__ == "__main__":
     # Define command line arguments.
-    parser = argparse.ArgumentParser(description="Run simulations of search "
-                                                 "for the N-K Problem using "
-                                                 "various search algorithms.")
-    parser.add_argument("-v", "--verbose", action='store_true',
-                        help="more verbose output")
-    parser.add_argument("-a", "--a-star", action='store_true',
-                        help="run simulations using A* Search")
-    parser.add_argument("-b", "--bounded-a-star", action='store_true',
-                        help="run simulations using Bounded A* Search")
-    parser.add_argument("-l", "--local-beam", action='store_true',
-                        help="run simulations using Local Beam Search")
-    parser.add_argument("--num-sims", type=parse_positive_int,
-                        default=defaults['num_sims'],
-                        help="total number of simulations to run")
-    parser.add_argument("-n", "--vehicles", type=parse_positive_int,
-                        default=defaults['n'],
-                        help="number of cars for each simulation")
-    parser.add_argument("-k", "--packages", type=parse_positive_int,
-                        default=defaults['k'],
-                        help="number of packages for each simulation")
-    parser.add_argument("-m", "--locations", type=parse_positive_int,
-                        default=defaults['m'],
-                        help="number of locations in the input map")
-    parser.add_argument("--heuristic", default=defaults['h_name'],
-                        choices=["zero", "undelivered", "scaled", "sum"],
-                        help="heuristic function to use")
-    parser.add_argument("--bound", type=parse_bound, default=defaults['bound'],
-                        help="causes Bounded A* to keep only the best BOUND "
-                             "number of successors for any given state; can "
-                             "be either a percentage of successors to keep "
-                             "(if between 0 and 1, both exclusive), or the "
-                             "maximum number of successors to keep (if an "
-                             "integer 1 or greater); if 0 or less, keep all "
-                             "successors, like regular A*")
-    parser.add_argument("--k-limit", type=parse_positive_int,
-                        default=defaults['k_limit'],
-                        help="causes Local Beam Search to consider only the "
-                             "best (K_LIMIT - 1) successors of any given "
-                             "state at each level of recursion")
+    _parser = argparse.ArgumentParser(description="Run simulations of search "
+                                                  "for the N-K Problem using "
+                                                  "various search algorithms.")
+    _parser.add_argument("-v", "--verbose", action='store_true',
+                         help="more verbose output")
+    _parser.add_argument("-a", "--a-star", action='store_true',
+                         help="run simulations using A* Search")
+    _parser.add_argument("-b", "--bounded-a-star", action='store_true',
+                         help="run simulations using Bounded A* Search")
+    _parser.add_argument("-l", "--local-beam", action='store_true',
+                         help="run simulations using Local Beam Search")
+    _parser.add_argument("--num-sims", type=parse_positive_int,
+                         default=defaults['num_sims'],
+                         help="total number of simulations to run")
+    _parser.add_argument("-n", "--vehicles", type=parse_positive_int,
+                         default=defaults['n'],
+                         help="number of cars for each simulation")
+    _parser.add_argument("-k", "--packages", type=parse_positive_int,
+                         default=defaults['k'],
+                         help="number of packages for each simulation")
+    _parser.add_argument("-m", "--locations", type=parse_positive_int,
+                         default=defaults['m'],
+                         help="number of locations in the input map")
+    _parser.add_argument("--heuristic", default=defaults['h_name'],
+                         choices=["zero", "undelivered", "scaled", "sum"],
+                         help="heuristic function to use")
+    _parser.add_argument("--bound", type=parse_bound, default=defaults['bound'],
+                         help="causes Bounded A* to keep only the best BOUND "
+                              "number of successors for any given state; can "
+                              "be either a percentage of successors to keep "
+                              "(if between 0 and 1, both exclusive), or the "
+                              "maximum number of successors to keep (if an "
+                              "integer 1 or greater); if 0 or less, keep all "
+                              "successors, like regular A*")
+    _parser.add_argument("--k-limit", type=parse_positive_int,
+                         default=defaults['k_limit'],
+                         help="causes Local Beam Search to consider only the "
+                              "best (K_LIMIT - 1) successors of any given "
+                              "state at each level of recursion")
 
     # Parse command line arguments.
-    args = parser.parse_args()
-    verbose = args.verbose
-    a_star = args.a_star
-    bounded_a_star = args.bounded_a_star
-    local_beam = args.local_beam
-    num_sims = args.num_sims
-    n = args.vehicles
-    k = args.packages
-    m = args.locations
-    h_name = args.heuristic
-    h = heuristics[h_name]
-    bound = args.bound
-    k_limit = args.k_limit
+    _args = _parser.parse_args()
+    _verbose = _args.verbose
+    _a_star = _args.a_star
+    _bounded_a_star = _args.bounded_a_star
+    _local_beam = _args.local_beam
+    _num_sims = _args.num_sims
+    _n = _args.vehicles
+    _k = _args.packages
+    _m = _args.locations
+    _h_name = _args.heuristic
+    _h = heuristics[_h_name]
+    _bound = _args.bound
+    _k_limit = _args.k_limit
 
-    if not a_star and not bounded_a_star and not local_beam:
-        raise parser.error("at least one of -a, -b, or -l must be given")
+    if not _a_star and not _bounded_a_star and not _local_beam:
+        raise _parser.error("at least one of -a, -b, or -l must be given")
 
     # Run the simulations and record simulation results.
-    name = "sims" + str(num_sims) + ".n" + str(n) + ".k" + str(k) + ".m" + \
-           str(m) + "." + h_name + "."
+    _names = get_file_names(_num_sims, _n, _k, _m, _h_name, _bound, _k_limit)
 
-    if verbose and a_star:
+    if _verbose and _a_star:
         print("Regular A* simulations.")
-    if a_star:
-        data_a_star = a_star_simulations(n, k, m, h, num_sims, verbose)
-        utils.dump_json_data(name + "a_star", data_a_star)
-        utils.plot_results(name + "a_star", data_a_star)
+    if _a_star:
+        data_a_star = a_star_simulations(_n, _k, _m, _h, _num_sims, _verbose)
+        utils.dump_json_data(_names['a_star'], data_a_star)
+        utils.plot_results(_names['a_star'], data_a_star)
 
-    if verbose and bounded_a_star:
+    if _verbose and _bounded_a_star:
         print("Bounded A* simulations.")
-    if bounded_a_star:
-        data_bounded_a_star = bounded_a_star_simulations(n, k, m, h, num_sims,
-                                                         bound, verbose)
-        bound_name = name + "bound" + str(bound) + ".bounded_a_star"
-        utils.dump_json_data(bound_name, data_bounded_a_star)
-        utils.plot_results(bound_name, data_bounded_a_star)
+    if _bounded_a_star:
+        data_bounded_a_star = bounded_a_star_simulations(_n, _k, _m, _h,
+                                                         _num_sims, _bound,
+                                                         _verbose)
+        utils.dump_json_data(_names['bounded_a_star'], data_bounded_a_star)
+        utils.plot_results(_names['bounded_a_star'], data_bounded_a_star)
 
-    if verbose and local_beam:
+    if _verbose and _local_beam:
         print("Local Beam Search simulations.")
-    if local_beam:
-        data_local_beam = local_beam_simulations(n, k, m, h, num_sims, k_limit,
-                                                 verbose)
-        local_beam_name = name + "k_limit" + str(k_limit) + ".local_beam"
-        utils.dump_json_data(local_beam_name, data_local_beam)
-        utils.plot_results(local_beam_name, data_local_beam)
+    if _local_beam:
+        data_local_beam = local_beam_simulations(_n, _k, _m, _h, _num_sims,
+                                                 _k_limit, _verbose)
+        utils.dump_json_data(_names['local_beam'], data_local_beam)
+        utils.plot_results(_names['local_beam'], data_local_beam)
