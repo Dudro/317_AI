45f9c590600dd64c7fbdb26671d560c1fd226fb6
Gaurav Arora (gaa721) <gaurav.arora@usask.ca>
2017-02-05 19:02:28 -0600

    Local beam search implemented

--- localbeam.py
+++ localbeam.py
@@ -1,7 +1,7 @@
 from queue import PriorityQueue
 
 
-def local_beam_search(state, is_goal, trans_op, f, k_limit=20):
+def greedy_search(state, is_goal, trans_op, f, k_limit=20):
     """
     if is_goal(state) then
         return state as a goal state
@@ -28,10 +28,10 @@ def local_beam_search(state, is_goal, trans_op, f, k_limit=20):
     :type k_limit: int
     :rtype: X (a goal state), integral
     """
-    return _local_beam_search_helper(0, state, is_goal, trans_op, f, k_limit)
+    return _greedy_search_helper(0, state, is_goal, trans_op, f, k_limit)
 
 
-def _local_beam_search_helper(counter, state, is_goal, trans_op, f, k_limit):
+def _greedy_search_helper(counter, state, is_goal, trans_op, f, k_limit):
     """
     A helper method that counts the number of nodes expanded, where 'counter'
     records the number of nodes expanded so far.
@@ -66,3 +66,51 @@ def _local_beam_search_helper(counter, state, is_goal, trans_op, f, k_limit):
                                                       trans_op, f, k_limit):
                 counter = exp
                 yield sol, counter
+
+
+def local_beam_search(state, is_goal, trans_op, f, k_limit=20):
+    """
+    if is_goal(state) then
+        return state as a goal state
+    else
+        candidates = trans_op(state)
+        choose (k-1) states, x, from candidates with the lowest f(x) values
+        recursively apply local_beam_search on each of those states
+
+    :param state: a current state
+    :type state: X, where X is the argument type of is_goal, trans_op, and f
+    :param is_goal: a function that takes a state, x, of type X, and
+        returns a boolean indicating whether x is a goal state
+    :type is_goal: X => bool, where X is any state type
+    :param trans_op: a function that takes a state, x, of type X, and
+        returns a iterable over type X containing all successor states of x
+    :type trans_op: X => list(X), where X is any state type
+    :param f: a function that takes a state, x, of type X, and computes
+        f(x) = g(x) + h(x), where g(x) is the cost so far from the initial
+        state to x, and h(x) is the estimated remaining cost from x to a
+        goal state
+    :type f: X => float, where X is any state type
+    :param k_limit: the number of successor states that will being considered
+        minus 1. Default: 20.
+    :type k_limit: int
+    :rtype: X (a goal state), integral
+    """
+    counter = 0
+    candidates = PriorityQueue()
+    candidates.put((f(state),counter, state))
+    while not candidates.empty():
+        if len(candidates.queue) > k_limit:
+            temp_candidate = PriorityQueue()
+            for i in range(0,k_limit):
+                temp_candidate.put(candidates.get())
+            candidates = temp_candidate
+        temp_candidate = PriorityQueue()
+        while not candidates.empty():
+            s = candidates.get()
+            new_candidates = trans_op(s[2])
+            for candidate in new_candidates:
+                counter += 1
+                if is_goal(candidate):
+                    yield candidate, counter
+                temp_candidate.put((f(candidate),counter, candidate))
+        candidates = temp_candidate
--- test_localbeam.py
+++ test_localbeam.py
@@ -1,9 +1,9 @@
-from localbeam import local_beam_search
+from localbeam import local_beam_search2
 from test_astar import *
 
 if __name__ == "__main__":
     # Generate (lazily) every solution (path) from Arad to Bucharest.
     print("Local Beam Search")
-    for sol, count in local_beam_search(GraphState(2, 0, [2]), at_bucharest,
-                                        trans_op, f, 5):
-        print(count, sol.get_cost_so_far(), solution.get_path_so_far())
+    for sol, count in local_beam_search2(GraphState(2, 0, [2]), at_bucharest,
+                                        trans_op, f, ):
+        print(count, sol.get_cost_so_far(), sol.get_path_so_far())
